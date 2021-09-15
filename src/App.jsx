2687340119fe41bbd8eecb03b7ab34c57ead1801
caarlosdamian <dapecarlos@gmail.com>
2021-09-14 22:57:20 -0500

    Working on styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import "./App.css";
 import Hills from "./images/pattern-hills.svg";
 import Starts from "./images/bg-stars.svg";
+import Facebook from "./images/facebook.svg";
+import Instagram from "./images/instagram.svg";
+import Pinterest from "./images/pinterest.svg";
 
 function App() {
   return (
@@ -9,7 +12,7 @@ function App() {
         <div className="starts">
           <img src={Starts} alt="" />
         </div>
-        <h1 >We're launching soon</h1>
+        <h1>We're launching soon</h1>
         <div className="countDown">
           <div>
             <h3>Days</h3>
@@ -28,8 +31,21 @@ function App() {
             <span>Time</span>
           </div>
         </div>
+        <div className="socials">
+          <ul>
+            <li>
+              <img src={Facebook} alt="Facebook" />
+            </li>
+            <li>
+              <img src={Instagram} alt="Instagram" />
+            </li>
+            <li>
+              <img src={Pinterest} alt="Pinteres" />
+            </li>
+          </ul>
+        </div>
         <footer className="hills">
-          <img src={Hills} alt='hills' />
+          <img src={Hills} alt="hills" />
         </footer>
       </div>
     </div>
